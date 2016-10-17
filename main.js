9c1baa3a0c2cfa0c8f36c55897cfcdae2d086da1
Muneer PP <abulmuneer22@gmail.com>
2016-10-17 22:56:45 +0530

    push

--- main.js
+++ main.js
@@ -58,59 +58,18 @@ app.get('/search/:origin/:destination', function (req, res) {
 
 
 
-app.get('/getCities', function (req, res) {
+app.get('/getCities/:budget', function (req, res) {
    
-    axios.get("https://nomadlist.com/api/v2/filter/city?c=5&f1_target=long_term_cost_in_usd&f1_type=lt&f1_max=3000&f2_target=temperatureC&f2_type=lt&f2_max=20&f3_target=internet_speed&f3_type=gt&f3_min=15&f4_target=nightlife&f4_type=gt&f4_min=3&f5_target=region&f5_type=em&f5_value=Asia&s=nomad_score&o=desc")
+    var budget = req.params.budget
+    axios.get("https://nomadlist.com/api/v2/filter/city?c=5&f1_target=long_term_cost_in_usd&f1_type=lt&f1_max="+budget+"&f2_target=temperatureC&f2_type=lt&f2_max=20&f3_target=internet_speed&f3_type=gt&f3_min=15&f4_target=nightlife&f4_type=gt&f4_min=3&f5_target=region&f5_type=em&f5_value=Asia&s=nomad_score&o=desc")
     .then(function(response){
     
     var city_1 = response.data.slugs[0]
     var city_2 = response.data.slugs[1]
     
     res.json([
-  {
-    "attachment":{
-      "type":"template",
-      "payload":{
-        "template_type":"generic",
-        "elements":[
-          {
-            "title":"Classic White T-Shirt",
-            "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
-            "subtitle":"Soft white cotton t-shirt is back in style",
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
-                "title":"View Item"
-              },
-              {
-                "type":"web_url",
-                "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
-                "title":"Buy Item"
-              }
-            ]
-          },
-          {
-            "title":"Classic Grey T-Shirt",
-            "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
-            "subtitle":"Soft gray cotton t-shirt is back in style",
-            "buttons":[
-              {
-                "type":"web_url",
-                "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
-                "title":"View Item"
-              },
-              {
-                "type":"web_url",
-                "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
-                "title":"Buy Item"
-              }
-            ]
-          }
-        ]
-      }
-    }
-  }
+  {"text": city_1},
+  {"text": city_2}
 ])
     });
 
