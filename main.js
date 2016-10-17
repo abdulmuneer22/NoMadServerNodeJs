454ad4ffb8aa25fef52a01646c0a3942c3e4996c
Muneer PP <abulmuneer22@gmail.com>
2016-10-17 20:21:42 +0530

    push

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app = express();
 
+var axios = require('axios')
+
+
+
+
+/*********************** */
+
 var fs = require("fs");
 
 app.get('/', function (req, res) {
@@ -15,45 +22,37 @@ var output = [
 
 
 var __dirname = "db"
-
-app.get('/list_users', function (req, res) {
-   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( data );
-   });
-})
-
-
-//method to get destinations and origin
-
-app.get('/search/:origin/:destination', function (req, res) {
-   console.log(req.param)
-})
-
-
-app.get('/search', function (req, res) {
-   console.log("Show out put for search API");
-   res.send('search API');
-})
-
-app.get('/search_destinations', function (req, res) {
-   console.log("Show out put for search API");
-   res.send(output);
-})
-
-/*
-app.get('/search_destinations', function (req, res) {
-   fs.readFile( __dirname + "/" + "destinations.json", 'utf8', function (err, data) {
-      console.log( data );
-      res.end( output );
-   });
-})
-*/
-
-
 var server = app.listen(8081, function () {
    var host = server.address().address
    var port = server.address().port
    
    console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
+
+app.get('/search/:origin/:destination', function (req, res) {
+   //console.log(req.param)
+   var origin = req.params.origin
+   var destination = req.params.destination
+   //res.end(origin +" "+ destination)
+
+    var config = {
+    headers: {'X-Mashape-Key': 'n6R9kUKpylmshxI5dWxq1u7HmLQIp1hYPqLjsnyFMjlJo3HN2O'}
+    };
+
+    var cardOutput = [
+
+    {"text": "Here is a list of results you may be interested in!"},
+    {"text": "How can I help you?"}
+]
+
+    axios.get("https://rome2rio12.p.mashape.com/Search?dName=London&oName=Cairo", config)
+    .then(function(response){
+    console.log(response.data.places)
+    //res.end("Origin : " + response.data.places[0].name + "\nDestination : " + response.data.places[1].name)
+    res.end(cardOutput)
+    });
+
+   //res.end( "origin is " + origin + "  destination is  " + destination );
 })
