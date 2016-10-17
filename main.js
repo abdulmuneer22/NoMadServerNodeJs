39fc64c142309a3a28fb638580b6d5924aae2666
Muneer PP <abulmuneer22@gmail.com>
2016-10-17 14:38:10 +0530

    changed json

--- main.js
+++ main.js
@@ -7,7 +7,42 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
+var output = [{
+	"attachment": {
+		"type": "template",
+		"payload": {
+			"template_type": "generic",
+			"elements": [
+                
+            {   //Starts
+				"title": "Top Selling Rain Suits - Clearance Sale",
+				"image_url": "https://fd379b09dfe5fd258cee-4c7efcc0fa50ed2f0ba8ecc23dd2f42d.ssl.cf1.rackcdn.com/164777-939-list.jpg",
+				"subtitle": "Get Top Selling Rain Suits - Clearance Sale at Starts @ Rs.189",
+				"buttons": 
+                
+                [{
+                    //Button list
+					"type": "web_url",
+					"title": "Shop Now",
+					"url": "http://www.amazon.in/"
+				}]
 
+                // Ends Here
+			}, 
+            
+            {
+				"title": " Custom Title ",
+				"image_url": "https://fd379b09dfe5fd258cee-4c7efcc0fa50ed2f0ba8ecc23dd2f42d.ssl.cf1.rackcdn.com/161543-e12-list.jpg",
+				"subtitle": "Get Classic Aviators \u0026 Stylish Wayfarer Unisex Sunglasses at 129",
+				"buttons": [{
+					"type": "web_url",
+					"title": "Shop Now",
+					"url": "http://www.amazon.in/"
+				}]
+			}]
+		}
+	}
+}]
 var __dirname = "db"
 
 app.get('/list_users', function (req, res) {
@@ -26,7 +61,7 @@ app.get('/search', function (req, res) {
 app.get('/search_destinations', function (req, res) {
    fs.readFile( __dirname + "/" + "destinations.json", 'utf8', function (err, data) {
       console.log( data );
-      res.end( data );
+      res.end( output );
    });
 })
 
