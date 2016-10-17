1e5bd04e9d5c548b8d605e2d43d65e50cc9f10dc
Muneer PP <abulmuneer22@gmail.com>
2016-10-17 14:41:13 +0530

    changed json

--- main.js
+++ main.js
@@ -12,8 +12,7 @@ var output = [{
 		"type": "template",
 		"payload": {
 			"template_type": "generic",
-			"elements": [
-            {
+			"elements": [{
 				"title": "Top Selling Rain Suits - Clearance Sale",
 				"image_url": "https://fd379b09dfe5fd258cee-4c7efcc0fa50ed2f0ba8ecc23dd2f42d.ssl.cf1.rackcdn.com/164777-939-list.jpg",
 				"subtitle": "Get Top Selling Rain Suits - Clearance Sale at Starts @ Rs.189",
@@ -22,9 +21,7 @@ var output = [{
 					"title": "Shop Now",
 					"url": "http://www.amazon.in/"
 				}]
-			}, 
-            
-            {
+			}, {
 				"title": " Custom Title ",
 				"image_url": "https://fd379b09dfe5fd258cee-4c7efcc0fa50ed2f0ba8ecc23dd2f42d.ssl.cf1.rackcdn.com/161543-e12-list.jpg",
 				"subtitle": "Get Classic Aviators \u0026 Stylish Wayfarer Unisex Sunglasses at 129",
