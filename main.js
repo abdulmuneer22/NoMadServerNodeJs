67deada327b6028992841c6aac40d8a462219122
Muneer PP <abulmuneer22@gmail.com>
2016-10-17 14:43:21 +0530

    changed json

--- main.js
+++ main.js
@@ -48,14 +48,19 @@ app.get('/search', function (req, res) {
    res.send('search API');
 })
 
+app.get('/search_destinations', function (req, res) {
+   console.log("Show out put for search API");
+   res.send(output);
+})
 
+/*
 app.get('/search_destinations', function (req, res) {
    fs.readFile( __dirname + "/" + "destinations.json", 'utf8', function (err, data) {
       console.log( data );
       res.end( output );
    });
 })
-
+*/
 
 
 var server = app.listen(8081, function () {
