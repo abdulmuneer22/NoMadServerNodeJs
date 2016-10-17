3192652dd99933b974b3b07206ef165775ad4772
Muneer PP <abulmuneer22@gmail.com>
2016-10-17 14:39:28 +0530

    changed json

--- main.js
+++ main.js
@@ -13,21 +13,15 @@ var output = [{
 		"payload": {
 			"template_type": "generic",
 			"elements": [
-                
-            {   //Starts
+            {
 				"title": "Top Selling Rain Suits - Clearance Sale",
 				"image_url": "https://fd379b09dfe5fd258cee-4c7efcc0fa50ed2f0ba8ecc23dd2f42d.ssl.cf1.rackcdn.com/164777-939-list.jpg",
 				"subtitle": "Get Top Selling Rain Suits - Clearance Sale at Starts @ Rs.189",
-				"buttons": 
-                
-                [{
-                    //Button list
+				"buttons": [{
 					"type": "web_url",
 					"title": "Shop Now",
 					"url": "http://www.amazon.in/"
 				}]
-
-                // Ends Here
 			}, 
             
             {
