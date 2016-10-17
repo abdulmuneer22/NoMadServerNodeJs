a62fc30774becf443ff5ac83ee92cfd6445c52ad
Muneer PP <abulmuneer22@gmail.com>
2016-10-17 23:03:30 +0530

    push

--- main.js
+++ main.js
@@ -64,7 +64,15 @@ app.get('/getCities/:budget', function (req, res) {
     axios.get("https://nomadlist.com/api/v2/filter/city?c=5&f1_target=long_term_cost_in_usd&f1_type=lt&f1_max="+budget+"&f2_target=temperatureC&f2_type=lt&f2_max=20&f3_target=internet_speed&f3_type=gt&f3_min=15&f4_target=nightlife&f4_type=gt&f4_min=3&f5_target=region&f5_type=em&f5_value=Asia&s=nomad_score&o=desc")
     .then(function(response){
     
-    var city_1 = response.data.slugs[0]
+    var city_1 = ''
+
+    if(!response.data.slugs[0]){
+        var city_1 = "No Results Found"
+    }else{
+        var city_1 = response.data.slugs[0]
+        
+    }
+    
     var city_2 = response.data.slugs[1]
     
     res.json([
