0637476141dc2baa89c58f77d300d1286febca1a
System Administrator <root@Muneers-MacBook-Pro.local>
2016-10-26 17:07:30 +0530

    fixed async call isseu

--- main.js
+++ main.js
@@ -2,11 +2,10 @@
 
 var express = require('express');
 var app = express();
-
+var i = 10;
 var axios = require('axios')
 var fs = require("fs");
-
-
+var querystring = require('querystring')
 
 var server = app.listen(8080, function () {
    var host = server.address().address
@@ -15,207 +14,312 @@ var server = app.listen(8080, function () {
    console.log("Example app listening at http://%s:%s", host, port)
 })
 
+var firebase = require('firebase')
+
+
 /************************************* */
 
+app.get('/firebase/login/',function(req,res){
+var config = {
+    apiKey: "AIzaSyDddCONm5ST3jEy3GX21a-MUYx7cx8A6SQ",
+    authDomain: "chatfueldb.firebaseapp.com",
+    databaseURL: "https://chatfueldb.firebaseio.com",
+    storageBucket: "chatfueldb.appspot.com",
+    messagingSenderId: "305204955026"
+};
 
 
-// API Call to make when two cities are known 
-// endpoint : server:8080/getPrice/:origin/:destination'
-//result {"text" : 'Price'}
+firebase.initializeApp(config);
 
-app.get('/getPrice/:Origin/:Destination',function(req,res){
-  var Origin = req.params.Origin
-  var Destination =req.params.Destination
-
-  //API headers
-  var config = {
-    headers: {'X-Mashape-Key': 'n6R9kUKpylmshxI5dWxq1u7HmLQIp1hYPqLjsnyFMjlJo3HN2O'}
-  };
-
-  axios.get("https://rome2rio12.p.mashape.com/Search?dName="+Destination+"&oName="+Origin, config)
-  .then(function(response){
-  res.json([
-        {"text" : response.data.routes[0].indicativePrice.price}])
-  })
+res.send("Logged In")
+})
 
 
-});
+/********Firebase get data****************** */
+app.get('/getData/:Origin/:Destination',function(req,res){
+console.log("get data")
+var Origin = req.params.Origin
+var Destination = req.params.Destination
+// Login to firebase
+// user name : muneer.pp@outlook.com
+//password : 12345test
 
 
-/* API Call to get Cities based on preferances 
-// endpoint : server:8080/getCities/:budget/:climate'
-//result list of top five cities
-//result [
-//    {"text" : 'city_1'},
-//    {"text" : 'city_2'},
-//    {"text" : 'city_3'},
-//    {"text" : 'city_4'},
-//    {"text" : 'city_5'},
-//  ]
-
-*/
-
-var getPrice = (Origin,Destination) => {
-  // Price
-  var Price = ""
-
-  //API headers
-  var config = {
-    headers: {'X-Mashape-Key': 'n6R9kUKpylmshxI5dWxq1u7HmLQIp1hYPqLjsnyFMjlJo3HN2O'}
-  };
-
-  axios.get("https://rome2rio12.p.mashape.com/Search?dName="+Destination+"&oName="+Origin, config)
-  .then((response)=>{
-    console.log(response.data.routes[0].indicativePrice.price)
-    Price = response.data.routes[0].indicativePrice.price
-  })
-
-  return Price;
+firebase.auth().signInWithEmailAndPassword("muneer.pp@outlook.com","12345test")
+.then((result)=>{
+    //console.log(result)
+},
+(error)=>{
+    //console.log(error)
 
 }
 
+)
 
-app.get('/PriceToACity/:Destination/:Origin',function(req,res){
-var _Price =  getPrice(req.params.Destination,req.params.Origin)
+var database = firebase.database();
+var config = {headers: {'X-Mashape-Key': 'VIueGJ2zcQmsh56E2bzQS1d05H2zp10vsKTjsntboUHkGuuUfj'}};
 
-console.log(_Price)
-
-
-})
+var DestinationList = require('./CityNames/Hot.json')
 
 
+DestinationList.forEach((child)=>{
+    axios.get("https://rome2rio12.p.mashape.com/Search?dName="+child.CITYNAME+"&oName="+Origin, config)
+    .then((response=>{
 
+        var price = response.data.routes[0].indicativePrice.price
+        console.log(child.CITYNAME)
+        var Destination = child.CITYNAME
+        firebase.database().ref('chatfuel/cities/'+Origin+'/'+Destination).set({
+            "Name" : Destination,
+            "Price" : price
+        })
 
-
-
-app.get('/getCities/:budget/:climate/:Origin', function (req, res) {
-  var budget = req.params.budget
-  var climate = req.params.climate
-  var OriginCity = req.params.Origin
-  var City_1_Price = ""
-  var City_2_Price = ""
-  var City_3_Price = ""
-  var City_4_Price = ""
-  var City_5_Price = ""
-
-  var city_1 = ""
-  var city_2 = ""
-  var city_3 = ""
-  var city_4 = ""
-  var city_5 = ""
-  
-  var Destination = "London"
-
-  // rome2rio12 api header
-  var config = {headers: {'X-Mashape-Key': 'n6R9kUKpylmshxI5dWxq1u7HmLQIp1hYPqLjsnyFMjlJo3HN2O'}};
-
-  switch(climate){
-        case 'COLD' :
-        var climateFilter = "f2_type=lt&f2_max=20"
-        //console.log(climateFilter)
-
-        case 'MILD':
-        var climateFilter = "bt&f2_min=16&f2_max=25"
-
-        case 'WARM':
-        var climateFilter = "f2_type=gt&f2_min=21"
-
-        case 'HOT' :
-        var climateFilter = "f1_type=gt&f1_min=30"
-
-        default :
-        var climateFilter = "f2_type=lt&f2_max=20"
-        }
-
-//API CALL
-axios.get("https://nomadlist.com/api/v2/filter/city?c=2&f1_target=long_term_cost_in_usd&f1_type=lt&f1_max="+budget+"&f2_target=temperatureC&"+climateFilter+"&s=nomad_score&o=desc")
-.then(
-  function(response){
-
-
-//get first five cities
-city_1 = response.data.slugs[0]
-city_2 = response.data.slugs[1]
-city_3 = response.data.slugs[2]
-city_4 = response.data.slugs[3]
-city_5 = response.data.slugs[4]
-
-
-  
-})
-.then(function(){
-
-//make rome2rio12 call Here
-
-console.log("Make rome2rio12 API Call Now")
-
-    axios.get("https://rome2rio12.p.mashape.com/Search?dName="+city_1+"&oName="+OriginCity, config)
-    .then(function(response){
-      
-      City_1_Price = response.data.routes[0].indicativePrice.price + "$ "
-
-      // Get Price for second city 
-      axios.get("https://rome2rio12.p.mashape.com/Search?dName="+city_2+"&oName="+OriginCity, config)
-      .then(function(response){
-        City_2_Price = response.data.routes[0].indicativePrice.price + "$ "
-          // get price for 3rd city Here
-              axios.get("https://rome2rio12.p.mashape.com/Search?dName="+city_3+"&oName="+OriginCity, config)
-              .then(function(response){
-                City_3_Price = response.data.routes[0].indicativePrice.price + "$ "
-
-                    // get Price for 4th City Now
-                        axios.get("https://rome2rio12.p.mashape.com/Search?dName="+city_4+"&oName="+OriginCity, config)
-                        .then(function(response){
-            
-                            City_4_Price = response.data.routes[0].indicativePrice.price + "$ "
-                            
-                              // Ge City 5 Price Now
-                                axios.get("https://rome2rio12.p.mashape.com/Search?dName="+city_5+"&oName="+OriginCity, config)
-                                .then(function(response){
-                                    City_5_Price = response.data.routes[0].indicativePrice.price + "$ "
-                                    
-                                      res.json([
-                                      {"text" : "Here Are Top Five Cities You May Be Interested In"},
-                                      {"text" : city_1},
-                                      {"text" : City_1_Price},
-                                      {"text" : city_2},
-                                      {"text" : City_2_Price},
-                                      {"text" : city_3},
-                                      {"text" : City_3_Price},
-                                      {"text" : city_4},
-                                      {"text" : City_4_Price},
-                                      {"text" : city_5},
-                                      {"text" : City_5_Price},
-                                      
-                                      ])
-
-                                })
-
-
-
-                            
-                        })
-
-                 
-
-                
-              })
-
-            
-      
-      })
-
-
-      
+    }))
+    .catch((error)=>{
+        console.log(error)
     })
+
+})
+
+/*
+firebase.database().ref('chatfuel/cities/'+Origin).update({
+    Destinations : {
+        Bangalore : 200,
+        Paris :3000
+
+    }
+
+})
+*/
+
+
+
+res.send("Get Data to Firebase" + Origin + Destination)
+
+
+})
+
+
+/********Firebase get data****************** */
+
+
+
+
+/********Firebase get data****************** */
+app.get('/Store/:Climate',function(req,res){
+//axios.get("http://nodeapi-92027.onmodulus.net/firebase/login")
+
+var Climate = req.params.Climate
+
+
+switch(Climate){
+    case 'COLD' :
+    var DestinationList = require('./CityNames/Cold.json')
+
+    case 'HOT':
+    var DestinationList = require('./CityNames/Hot.json')
+
+
+    case 'WARM':
+    var DestinationList = require('./CityNames/Warm.json')
+    
+    
+    
+    
+    
+}
+
+// Login to firebase
+// user name : muneer.pp@outlook.com
+//password : 12345test
+
+
+firebase.auth().signInWithEmailAndPassword("muneer.pp@outlook.com","12345test")
+.then((result)=>{
+    //console.log(result)
+},
+(error)=>{
+    //console.log(error)
+
+}
+
+)
+
+var database = firebase.database();
+
+var DestinationList = require('./CityNames/Hot.json')
+
+
+DestinationList.forEach((child)=>{
+
+    var Origin = child.CITYNAME
+    console.log(Origin)
+    axios.get("http://nodeapi-92027.onmodulus.net/getData/"+Origin+"/Paris")
+    
+
+})
+
+/*
+firebase.database().ref('chatfuel/cities/'+Origin).update({
+    Destinations : {
+        Bangalore : 200,
+        Paris :3000
+
+    }
+
+})
+*/
+
+
+res.send("Send !!")
+
+
+
+})
+
+
+/********Firebase get data****************** */
+
+app.get('/getCities/:Climate/:Budget/:OriginCity/',function(req,res){
+
+var Origin = req.params.OriginCity
+var Destination = req.params.Destination
+var Climate = req.params.Climate
+var Budget = req.params.Budget
+console.log("Budget : " + Budget)
+
+switch(Climate){
+    case 'HOT':
+    var DestinationList = require('./CityNames/Hot.json')
+    break;
+
+    case 'COLD':
+    var DestinationList = require('./CityNames/Cold.json')
+    break;
+    
+    case 'WARM':
+    var DestinationList = require('./CityNames/Warm.json')
+    break;
+
+    default : 
+    var DestinationList = require('./cityNames.json')
+    
+    
+}
+
+
+
+
+
+first()
+
+
+
+
+
+
+
+function first(){
+
+    var OutPut = [];
+    var apiCalls = 0
+    
+    //var OriginCity = "Cairo"
+    var max_budget = Budget
+    var config = {headers: {'X-Mashape-Key': 'VIueGJ2zcQmsh56E2bzQS1d05H2zp10vsKTjsntboUHkGuuUfj'}};
+
+    var i = 0
+    
+    DestinationList.forEach((child)=>{
+        
+        i = i+ 1
+
+        if(i < 5){
+        console.log(i)
+        axios.get("https://rome2rio12.p.mashape.com/Search?dName="+child.CITYNAME+"&oName="+Origin, config)
+        .then((response)=>{
+            
+                OutPut.push(
+            {
+                "text" : child.CITYNAME
+            },
+            {
+                "text" : response.data.routes[0].indicativePrice.price + " $" 
+            }
+        
+            )
+            
+            
+            console.log(response.data.routes[0].indicativePrice.price +":" +child.CITYNAME)
+            
+            if(i > 10){
+                res.json(OutPut)
+            }
+            
+
+               
+
+
+        }).catch((error)=>{
+            
+            console.log(error)
+            
+        })
+
+        
+        
+        
+        
+       
+
+
+            
+        }
+        
+        
+
+
+    })
+
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
     
 
 
 
+}
+
+
+function second(input){
+
+res.json(input)
+
+
+}
+    
+
+
 
 })
 
 
 
 
-})
+
+  
+ 
+ 
+  
+
+
+  
+
---
+++ newserver.js
@@ -0,0 +1,135 @@
+var express = require('express');
+var app = express();
+var i = 10;
+var axios = require('axios')
+var fs = require("fs");
+var querystring = require('querystring')
+
+var async = require("async");
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
+
+
+
+
+app.get('/api/:Origin/:Destination/:Climate',function(req,res){
+
+var Origin = req.params.Origin;
+var Destination = req.params.Destination
+var Climate = req.params.Climate
+var NomadURL ;
+var CityList = []
+var count = 0;
+var price;
+var data = []
+var response
+var config = {headers: {'X-Mashape-Key': 'n6R9kUKpylmshxI5dWxq1u7HmLQIp1hYPqLjsnyFMjlJo3HN2O'}};
+
+var NomadURL = require('./CityNames/Cold.json')
+//console.log(NomadURL)
+
+var a = NomadURL
+
+var f = function(arg,callback){
+    console.log(arg)
+    axios.get("https://rome2rio12.p.mashape.com/Search?dName="+arg.CITYNAME+"&oName="+Origin, config)
+    .then((response)=>{
+        //console.log(response.data.routes[0].indicativePrice.price)
+        console.log("Started")
+        data.push({
+            "CityName" : arg.CITYNAME,
+            "Price" : response.data.routes[0].indicativePrice.price
+        })
+    console.log("pushed")
+    })
+    .then(()=>{
+        callback();
+    })
+
+
+    
+}
+
+
+async.forEach(a,f,function(err){
+    console.log("done")
+    res.send(data)
+})
+
+
+
+/*
+NomadURL.forEach((child)=>{
+    var config = {
+    headers: {'X-Mashape-Key': 'n6R9kUKpylmshxI5dWxq1u7HmLQIp1hYPqLjsnyFMjlJo3HN2O'}
+    };
+    Origin = "London"
+    axios.get("https://rome2rio12.p.mashape.com/Search?dName="+child.CITYNAME+"&oName="+Origin, config)
+    .then((response)=>{
+        console.log(response.status)
+        if(response.status === 200){
+            data.push({
+                "CityName" : child.CITYNAME,
+                "Price" : response.data.routes[0].indicativePrice.price
+            })
+       
+            
+        }
+        
+    })
+
+
+})
+
+*/
+
+
+
+
+    
+    /*
+    async.series([
+        function(callback){
+            console.log("Call One")
+            PriceList.push({
+                "One" : "One"
+            })
+            callback();
+        },
+        function(callback){
+            console.log("Call Two")
+            PriceList.push({
+                "Two" : "Two"
+            })
+            callback();
+            
+            
+        },
+        function(callback){ 
+            console.log("Call Three")
+            PriceList.push({
+                "Three" : "Three"
+            })
+            callback();
+            
+            
+        }
+    ],function(err){
+        console.log("everything is done !!")
+        res.send(PriceList)
+    }
+    
+    );
+    
+    */
+})
+
+
+
+
